93eeef5f51dc2c0435e321a8ed200c907e715700
Raj Sathavara <sathawararaj26@gmail.com>
2026-02-21 01:25:28 +0530

    frontend deployment set-up

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/api',
+    baseURL: "https://moctail-movie-recommendation-2.onrender.com",
     headers: { 'Content-Type': 'application/json' },
 });
 
